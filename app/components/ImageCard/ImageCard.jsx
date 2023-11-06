0c940a5486a941f336abf1a8a8326c794f95bf5e
mohaimenur1 <mmrahmanrahi@gmail.com>
2023-11-06 07:13:56 +0600

    checked box functionality done

--- app/components/ImageCard/ImageCard.jsx
+++ app/components/ImageCard/ImageCard.jsx
@@ -1,61 +1,30 @@
 "use client";
 import React, { useState } from "react";
 
-import { Box, Card, Checkbox, Paper } from "@mui/material";
+import { Box, Card, Checkbox, Paper, Typography } from "@mui/material";
 
-const ImageCard = ({ img, count, setIsCount }) => {
-  // const effect = {
-  //   "&:hover": {
-  //     background: "rgba(0, 0, 0, 0.5)",
-  //     zIndex: 1,
-  //   },
-  // };
+const ImageCard = ({ img, count, setIsCount, i }) => {
+  const [checked, setChecked] = useState(false);
 
-  const [hovered, setHovered] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleMouseEnter = () => {
-    setHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    if (!isChecked) {
-      setHovered(false);
-    }
-  };
-
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-
-    console.log("isChecked", isChecked);
-    if (isChecked) {
-      setIsCount(count - 1);
-      setHovered(false);
-    } else {
-      setIsCount(count + 1);
-      setHovered(true);
-    }
+  const handleCheckboxChange = (i) => {
+    setChecked(!checked);
   };
 
   return (
-    <Card
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      sx={{ position: "relative" }}
-    >
-      {hovered && (
-        <Checkbox
-          checked={isChecked}
-          onChange={handleCheckboxChange}
+    <Box>
+      <Card sx={{ position: "relative" }}>
+        <input
+          type="checkbox"
+          onChange={() => handleCheckboxChange(i)}
           sx={{ position: "absolute", top: 5, right: 5 }}
         />
-      )}
-      <img
-        src={img.src}
-        style={{ width: "100%", height: "100%", zIndex: -1 }}
-        alt="image"
-      />
-    </Card>
+        <img
+          src={img.src}
+          style={{ width: "100%", height: "100%", zIndex: -1 }}
+          alt="image"
+        />
+      </Card>
+    </Box>
   );
 };
 
--- app/components/Images/Images.jsx
+++ app/components/Images/Images.jsx
@@ -2,24 +2,58 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
 import { useState } from "react";
 import ImageData from "../ImageData";
 import ImageCard from "../ImageCard/ImageCard";
 import { v4 as uuidv4 } from "uuid";
+import { Typography } from "@mui/material";
+import { Card, Checkbox, Paper } from "@mui/material";
 
 const Images = () => {
   const [images, setImages] = useState(ImageData);
   const [count, setIsCount] = useState(0);
+  const [checked, setChecked] = useState(false);
+
+  const handleCheckboxChange = (i) => {
+    const updatedImages = [...images];
+    // Toggle the checked state of the clicked card
+    updatedImages[i].checked = !updatedImages[i].checked;
+
+    // Update the count based on the checked state of all cards
+    const newCount = updatedImages.filter((img) => img.checked).length;
+    setIsCount(newCount);
+
+    // Update the images state to reflect the changes
+    setImages(updatedImages);
+  };
 
   return (
     <Box maxWidth="xl">
       <Typography>{count}</Typography>
       <Grid container spacing={2}>
-        {images.map((img) => {
+        {images.map((img, i) => {
           return (
             <Grid item md={2} key={uuidv4()}>
-              <ImageCard img={img} count={count} setIsCount={setIsCount} />
+              <Card sx={{ position: "relative" }}>
+                <Checkbox
+                  // type="checkbox"
+                  // value={checked}
+                  onChange={() => handleCheckboxChange(i)}
+                  sx={{ position: "absolute", top: 5, right: 5 }}
+                  checked={img.checked}
+                />
+                <img
+                  src={img.src}
+                  style={{ width: "100%", height: "100%", zIndex: -1 }}
+                  alt="image"
+                />
+              </Card>
+              {/* <ImageCard
+                img={img}
+                count={count}
+                setIsCount={setIsCount}
+                i={i}
+              /> */}
             </Grid>
           );
         })}
